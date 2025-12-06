dd933f8d5042d24f42aece2ab8e8bbf05996eb4c
Alejandro <amendozapan@unsa.edu.pe>
2025-12-05 20:53:21 -0500

    Ejercicio 06.2

--- ejercicio06.py
+++ ejercicio06.py
@@ -1,65 +1,45 @@
-import array
+import numpy as np
 import math
 
-lista = array.array('i', [10, 20, 30])
-
-def desviacion(lista, media):
-    lista = array.array('d', ((x - media) ** 2 for x in lista))
-    division = sum(lista) / len(lista)
-    desviacion = math.sqrt(division)
-    return desviacion
+lista = np.array([10, 20, 30])
 
 def calcular_norma(lista):
-    lista = array.array('d', ((x ** 2) for x in lista))
-    suma = math.sqrt(sum(lista))
-    return suma
+    return math.sqrt(np.sum(lista ** 2))
+
 
 def normalizar(lista, modo):
 
-    min = lista[0]
-    for i in range(len(lista)):
-        if lista[i] < min:
-            min = lista[i]
-
-    max = lista[0]
-    for i in range(len(lista)):
-        if lista[i] > max:
-            max = lista[i]
-
-    media = sum(lista) / len(lista)
-    desviacion_calculo = desviacion(lista, media)
+    minimo = lista.min()
+    maximo = lista.max()
+    media = lista.mean()
+    desviacion_calculo = lista.std()
     norma = calcular_norma(lista)
 
     try:
         match modo:
             case "minmax":
-                lista = array.array('d', ((x - min) / (max - min) for x in lista))
-                return lista
+                return (lista - minimo) / (maximo - minimo)
+
             case "zscore":
-                lista = array.array('d', ((x - media) / desviacion_calculo for x in lista))
-                return lista
+                return (lista - media) / desviacion_calculo
+
             case "unit":
-                lista = array.array('d', ((x / norma) for x in lista))
-                return lista
-            case _:
-                print("Opción no válida")
+                if norma == 0:
+                    raise ZeroDivisionError("norma = 0")
+                return lista / norma
 
     except ZeroDivisionError as e:
         print("Error:", e)
-        return None
 
 
 continuar = True
-
 while continuar:
-    modo = input("Ingrese una opción de modo o 'S' para salir: ")
+    modo = input("Ingrese una opcion de modo o 'S' para salir: ")
     if modo.lower() == "s":
-        print("Saliendo...")
         continuar = False
-        break
-
-    while modo.lower() not in ("minmax", "zscore", "unit"):
-        print("Opción no válida")
-        modo = input("Ingrese una opción de modo: ")
-
-    print(normalizar(lista, modo.lower()))
+        print("Saliendo")
+    else:
+        while (modo.lower() != "minmax" and modo.lower() != "zscore" and modo.lower() != "unit"):
+            print("Opción no válida")
+            modo = input("Ingrese una opcion de modo: ")
+        print(normalizar(lista, modo.lower()))
